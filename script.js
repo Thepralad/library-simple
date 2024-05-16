dc3a2b1f7555eba620b004f3710a8f5628bbfa47
thepralad <prgurung04@gmail.com>
2024-05-16 10:04:24 +0530

    Refactor Book cl ass to use more descriptive text for hasRead property

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Book(title, author, pages, hasRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.hasRead = hasRead ? 'has read' : 'not yet read';
+    this.hasRead = hasRead ? 'Already read' : 'Not yet read';
 }
 
 const container = document.getElementById('container');
@@ -11,20 +11,37 @@ const container = document.getElementById('container');
 function addBookToLibrary(title, author, pages, hasRead){
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
-    
-    const _book = document.createElement('div');
-    const _title = document.createElement('h3');
-    const _author = document.createElement('p');
-    const _pages = document.createElement('p');
-    _title.textContent = title;
-    _author.textContent = author;
-    _pages.textContent = `${pages} pages`;
-    _book.appendChild(_title);
-    _book.appendChild(_author);
-    _book.appendChild(_pages);
-    container.appendChild(_book);
 }
 
+function displayBook(){
+    for (let i = 0; i < myLibrary.length; i++) {
+        const _book = document.createElement('div');
+        _book.classList.add('book');
+        const _title = document.createElement('h3');
+        const _author = document.createElement('p');
+        const _pages = document.createElement('p');
+        const _hasRead = document.createElement('p');
+        const _removeBtn = document.createElement('button')
 
+        _title.textContent = myLibrary[i].title;
+        _author.textContent = myLibrary[i].author;
+        _pages.textContent = `${myLibrary[i].pages} pages`;
+        _hasRead.textContent =  myLibrary[i].hasRead;
+        _removeBtn.textContent = 'Remove'
+        
+        _book.appendChild(_title);
+        _book.appendChild(_author);
+        _book.appendChild(_pages);
+        _book.appendChild(_hasRead);
+        _book.appendChild(_removeBtn);
 
+        container.appendChild(_book);
+    }
+}
+
+addBookToLibrary('Nigga', 'thisIsCool', 256, true);
+addBookToLibrary('hair', 'beardo', 234, false);
+addBookToLibrary('love', 'andrew tate', 76, true);
+
+displayBook();
 
